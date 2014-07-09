7da817442f9114ca7599b3af30f4154772c1e4d3
Daisuke Maki <lestrrat+github@gmail.com>
2014-07-09 18:13:45 +0900

    Annotate so I can start digging

--- main.go
+++ main.go
@@ -13,7 +13,10 @@ import (
 	"github.com/koron/gomigemo/migemo"
 )
 
+// Looks for possible dictionary directories.
+// We should expand this to more unix-y locations, too
 func dictdir() string {
+	// I want to change this to G*O*MIGEMO_DICTDIR
 	d := os.Getenv("GMIGEMO_DICTDIR")
 	if d != "" {
 		return d
@@ -36,6 +39,7 @@ func dictdir() string {
 
 var dictPath = flag.String("d", dictdir(), "Location to dictionary")
 
+// Does the grepping
 func grep(r io.Reader, re *regexp.Regexp) error {
 	buf := bufio.NewReader(r)
 	for {
@@ -79,23 +83,30 @@ func main() {
 		os.Exit(1)
 	}
 
+
+	// If there's only one arg, then we need to match against the input
 	if flag.NArg() == 1 {
 		if err = grep(os.Stdin, re); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-	} else {
-		for _, arg := range flag.Args()[1:] {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				os.Exit(1)
-			}
-			defer f.Close()
-			if err = grep(f, re); err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				os.Exit(1)
-			}
+
+		// We got here, we're fine.
+		return
+	}
+
+
+	// More than one arg. We must be searching against a file
+	for _, arg := range flag.Args()[1:] {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		if err = grep(f, re); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
 	}
 }
