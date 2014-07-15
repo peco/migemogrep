eff9f1eb2777d678f69f4b33d201d020e7b99645
mattn <mattn.jp@gmail.com>
2014-07-15 19:37:44 +0900

    Move expandArgs to work on windows only

--- grep_test.go
+++ grep_test.go
@@ -152,33 +152,3 @@ foobar
 		t.Fatalf("Should be %v but %v", expect, s)
 	}
 }
-
-func TestExpandArgs(t *testing.T) {
-	args := os.Args
-	defer func() {
-		os.Args = args
-	}()
-
-	os.Args = []string{"foo", "*_test.go"}
-	expandArgs()
-
-	expect := []string{"foo", "grep_test.go"}
-	if os.Args[0] != "foo" || os.Args[1] != "grep_test.go" {
-		t.Fatalf("Should be %v but %v", expect, os.Args)
-	}
-}
-
-func TestExpandArgsDashDash(t *testing.T) {
-	args := os.Args
-	defer func() {
-		os.Args = args
-	}()
-
-	os.Args = []string{"foo", "--", "*_test.go"}
-	expandArgs()
-
-	expect := []string{"foo", "*_test.go"}
-	if os.Args[0] != "foo" || os.Args[1] != "*_test.go" {
-		t.Fatalf("Should be %v but %v", expect, os.Args)
-	}
-}
