35f5fba296fb703acd66d0c6372f7a166f0a8666
mattn <mattn.jp@gmail.com>
2014-07-15 17:47:49 +0900

    Add test for --

--- grep_test.go
+++ grep_test.go
@@ -167,3 +167,18 @@ func TestExpandArgs(t *testing.T) {
 		t.Fatalf("Should be %v but %v", expect, os.Args)
 	}
 }
+
+func TestExpandArgsDashDash(t *testing.T) {
+	args := os.Args
+	defer func() {
+		os.Args = args
+	}()
+
+	os.Args = []string{"foo", "--", "*_test.go"}
+	expandArgs()
+
+	expect := []string{"foo", "*_test.go"}
+	if os.Args[0] != "foo" || os.Args[1] != "*_test.go" {
+		t.Fatalf("Should be %v but %v", expect, os.Args)
+	}
+}
